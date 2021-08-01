cc1bd27676a1edaa4d970bb08928231182358044
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2021-08-01 23:32:16 +0000

    modification du design

--- src/YaoJosue/morpion/Main.java
+++ src/YaoJosue/morpion/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Jeu de Morpion");
-        primaryStage.setScene(new Scene(root, 500, 550));
+        primaryStage.setScene(new Scene(root, 600, 550));
         primaryStage.show();
         primaryStage.setResizable(false);
     }
